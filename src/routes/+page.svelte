e89c3a826456ad177d09e805942a4a45b0c57df9
Ayush Kumar <94519836+Ayushkumar48@users.noreply.github.com>
2026-02-14 14:08:40 +0530

    feat: added anton font and name section in homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,20 @@
-<script>
-	import { Button } from '$lib/components/ui/button';
-</script>
+<div class="flex h-screen w-screen flex-col items-center px-10 py-8">
+	<div class="flex w-4xl flex-col gap-10 rounded-2xl bg-accent px-2 py-4 text-accent-foreground">
+		<h2 class="text-center text-5xl font-extrabold md:text-7xl lg:text-9xl">LATENCY SLAYER</h2>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<Button>Hi</Button>
+		<div>
+			<p class="w-2/5">
+				I build high-performance backend systems that power scalable, real-time applications. From
+				APIs to databases, I focus on reliability, security, and clean architecture that holds up in
+				production.
+			</p>
+		</div>
+		<div class="flex justify-end">
+			<p class="w-2/5 text-right">
+				With a strong interest in system design and performance optimization, I enjoy turning
+				complex ideas into simple, efficient solutions that deliver real value to users.
+			</p>
+		</div>
+	</div>
+	<div>wdew</div>
+</div>
